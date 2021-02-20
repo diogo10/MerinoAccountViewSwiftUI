eb132b67476898c8fb5fdcc762847d6def6d7591
Diogo Ribeiro <diogjp10@gmail.com>
2021-02-20 10:41:36 +0000

    wip

--- Sources/AccountViewMerino/AccountView.swift
+++ Sources/AccountViewMerino/AccountView.swift
@@ -1,47 +1,63 @@
 import SwiftUI
 
-public struct AccountView: View {
+public protocol MerinoAccountViewDelegate {
+    func accountClickItem(index: Int)
+}
+
+public struct MerinoAccountViewModelSection: Identifiable {
+    public var id = UUID()
+    var title: String
+    var items: [MerinoAccountViewModel]
+    
+    public init(title:String, items:[MerinoAccountViewModel]){
+        self.title = title
+        self.items = items
+    }
+}
+
+public struct MerinoAccountViewModel: Identifiable {
+    public var id = UUID()
+    var title:String
+    var subTitle:String
+    var imageName:String
+    var systemImage:String
+    
+    public init(title:String, subTitle:String, imageName: String, arrowImage: String){
+        self.title = title
+        self.subTitle = subTitle
+        self.imageName = imageName
+        self.systemImage = arrowImage
+    }
+}
+
+public struct MerinoAccountView: View {
     @State private var hasTimeElapsed = false
     
-    public init() {}
+    var delegate: MerinoAccountViewDelegate
+    var items: [MerinoAccountViewModelSection]
+    
+    public init(delegate: MerinoAccountViewDelegate, items: [MerinoAccountViewModelSection]) {
+        self.delegate = delegate
+        self.items = items
+    }
     
     public var body: some View {
-        NavigationView {
+        
+        List {
             
-            List {
-                
+            ForEach(items) { category in
                 Section(header: HStack {
-                    HeaderView(title: "Account")
+                    HeaderView(title: category.title)
                     Spacer()
-                }.background(Color.white).listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                ) {
-                    ItemView(item: ItemViewModel(title: "Diogo Ribeiro", subTitle: "diogjp10@gmail.com", imageName: "avatar", arrowImage: "pencil"))
-                    ItemView(item: ItemViewModel(title: "https//bit.ly/ADFGDGF", subTitle: "", imageName: "ic_browser", arrowImage: "link"), isLink: true)
+                }.background(Color.white).listRowInsets(EdgeInsets(top: -10, leading: 0, bottom: 0, trailing: 0))) {
+                    ForEach(category.items) { item in
+                        ItemView(item: item)
+                    }
                 }
-                
-                Section(header: HStack {
-                    HeaderView(title: "Actions")
-                    Spacer()
-                }.background(Color.white).listRowInsets(EdgeInsets(top: -10, leading: 0, bottom: 0, trailing: 0))
-                ) {
-                    ItemView(item: ItemViewModel(title: "Booking", subTitle: "", imageName: "avatar", arrowImage: "calendar.badge.plus"))
-                    ItemView(item: ItemViewModel(title: "Client", subTitle: "", imageName: "avatar", arrowImage: "person.crop.circle.badge.plus"))
-                }
-                
-                
-                Section(header: HStack {
-                    HeaderView(title: "Others")
-                    Spacer()
-                }.background(Color.white).listRowInsets(EdgeInsets(top: -10, leading: 0, bottom: 0, trailing: 0))
-                ) {
-                    ItemView(item: ItemViewModel(title: "Categories", subTitle: "0 categories", imageName: "avatar"))
-                    ItemView(item: ItemViewModel(title: "Services", subTitle: "0 actives, 0 inactive", imageName: "avatar"))
-                }
-                
-                
-            }.listStyle(GroupedListStyle())
-            .navigationBarHidden(true)
-        }
+            }
+            
+            
+        }.listStyle(GroupedListStyle()).navigationBarHidden(true)
     }
 }
 
@@ -57,17 +73,9 @@ struct HeaderView: View {
     }
 }
 
-struct ItemViewModel:Identifiable {
-    var id = UUID()
-    var title:String
-    var subTitle:String
-    var imageName:String
-    var arrowImage:String?
-}
-
 struct ItemView: View {
     
-    var item: ItemViewModel
+    var item: MerinoAccountViewModel
     var isLink:Bool = false
     
     var body: some View {
@@ -91,9 +99,7 @@ struct ItemView: View {
                 }
                 Spacer()
                 
-                if item.arrowImage != nil {
-                    Image(systemName: item.arrowImage!).frame(width: 24, height: 24)
-                }
+                Image(systemName: item.systemImage).frame(width: 24, height: 24)
                 
             }.padding(EdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 16))
             
--- Tests/AccountViewMerinoTests/AccountViewMerinoTests.swift
+++ Tests/AccountViewMerinoTests/AccountViewMerinoTests.swift
@@ -6,7 +6,7 @@ final class AccountViewMerinoTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AccountViewMerino().text, "Hello, World!")
+        //XCTAssertEqual(MerinoAccountView().text, "Hello, World!")
     }
 
     static var allTests = [
